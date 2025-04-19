ef3332dada04ffeabc5daf3c49a5d7129251b255
janLolen <163869597+janLolen@users.noreply.github.com>
2025-04-19 16:04:32 +0200

    minor fix

--- Minecraft/clone.bat
+++ Minecraft/clone.bat
@@ -2,5 +2,8 @@ robocopy ".\resource pack" "%appdata%\.minecraft\resourcepacks\poki pi sitelen p
 robocopy ".\resource pack" "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo" /E
 del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\assets\minecraft\textures\font\sitelen_telo.png"
 del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_pona.png"
+del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\pack_telo.png"
+del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\pack.png"
+rename "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\pack_telo.png" pack.png
 rename "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_telo.png" "sitelen_pona.png"
 pause
