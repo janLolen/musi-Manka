42e26065fdddbc77ad17b8d5be1755f46bae7403
janLolen <163869597+janLolen@users.noreply.github.com>
2025-04-19 15:44:56 +0200

    tidy

--- Minecraft/clone.bat
+++ Minecraft/clone.bat
@@ -1,7 +1,5 @@
-robocopy . "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona" /E
-robocopy . "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo" /E
-del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\clone.bat"
-del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\clone.bat"
+robocopy ".\resource pack" "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona" /E
+robocopy ".\resource pack" "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo" /E
 del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\assets\minecraft\textures\font\sitelen_telo.png"
 del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_pona.png"
 rename "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_telo.png" "sitelen_pona.png"
