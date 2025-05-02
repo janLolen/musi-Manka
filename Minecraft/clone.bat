f8f2d12c7216a026007af0b33ad2dd90da11fb9b
janLolen <163869597+janLolen@users.noreply.github.com>
2025-05-02 22:28:58 +0200

    ni la poki ni li kepeken ilo pona pi tenpo sin. nimi sin li lon. toki sike li lon.

--- Minecraft/clone.bat
+++ Minecraft/clone.bat
@@ -1,7 +1,11 @@
 robocopy ".\resource pack" "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona" /E
 robocopy ".\resource pack" "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo" /E
-del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\assets\minecraft\textures\font\sitelen_telo.png"
-del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_pona.png"
+rename "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\assets\minecraft\font\default_pona.json" default.json
+rename "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\font\default_telo.json" default.json
+del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\assets\minecraft\font\default_telo.json"
+del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\font\default_pona.json"
+del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\assets\minecraft\font\sitelen_telo.ttf"
+del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\font\sitelen_pona.ttf"
 del "%appdata%\.minecraft\resourcepacks\poki pi sitelen pona\pack_telo.png"
 del "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\pack.png"
 rename "%appdata%\.minecraft\resourcepacks\poki pi sitelen telo\pack_telo.png" pack.png
