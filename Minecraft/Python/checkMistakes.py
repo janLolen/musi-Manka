1e5375f66873fc0e31ae8ab9923d35f485c3b213
janLolen <163869597+janLolen@users.noreply.github.com>
2025-09-07 21:33:49 +0200

    starting new version

--- Minecraft/Python/checkMistakes.py
+++ Minecraft/Python/checkMistakes.py
@@ -5,7 +5,7 @@ with open('tok_Lasina.json', encoding='UTF-8') as lasina:
         las = json.load(lasina) # data that will be used only if not in the other one
         pon = json.load(pona) # all data here will be kept, this file is supposed to be missing entries
         for i in las:
-            if("sitelen" not in las[i] and "󱥠" in pon[i]):
+            if("linja" in las[i] and "󱤩" not in pon[i]):
                 print(i)
 
 input("sina luka e nena la sina pini e ilo ni")
